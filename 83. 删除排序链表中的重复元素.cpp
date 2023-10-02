e69501ac2951beea08e93d10f9e74a42122317be
yawn831 <143867885+yawn831@users.noreply.github.com>
2023-10-02 17:18:26 +0800

    Create 83. 删除排序链表中的重复元素.cpp

---
+++ 83. 删除排序链表中的重复元素.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==nullptr)
+        {
+            return head;
+        }
+        ListNode* fast=head;
+        ListNode* slow=head;
+        ListNode* p=head;
+        while(fast->next!=nullptr)
+        {
+            if(fast->val==fast->next->val)
+            {
+                fast=fast->next;
+            }
+            else
+            {
+                fast=fast->next;
+                slow=fast;
+                p->next=slow;
+                p=slow;
+            }
+        }
+        p->next=nullptr;
+        return head;
+    }
+};
