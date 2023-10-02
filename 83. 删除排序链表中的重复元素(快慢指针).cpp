7267c275d7d5ad47d6703138d1920fe48f4c47ea
yawn831 <143867885+yawn831@users.noreply.github.com>
2023-10-02 17:27:03 +0800

    Update 83. 删除排序链表中的重复元素(快慢指针).cpp

--- 83. 删除排序链表中的重复元素(快慢指针).cpp
+++ 83. 删除排序链表中的重复元素(快慢指针).cpp
@@ -26,3 +26,31 @@ public:
         return head;
     }
 };
+
+
+lass Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==nullptr)
+        {
+            return head;
+        }
+        ListNode* fast=head;
+        ListNode* p=head;
+        while(fast->next!=nullptr)
+        {
+            if(fast->val==fast->next->val)
+            {
+                fast=fast->next;
+            }
+            else
+            {
+                fast=fast->next;  
+                p->next=fast;
+                p=fast;
+            }
+        }
+        p->next=nullptr;
+        return head;
+    }
+};
