2803bdf3286ed9fd21af1cfe8be7ee38a6b9ae4d
yawn831 <143867885+yawn831@users.noreply.github.com>
2023-10-06 22:16:43 +0800

    Update 删除元素.cpp

--- 删除元素.cpp
+++ 删除元素.cpp
@@ -14,3 +14,21 @@ public:
         return b;
     }
 };
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int fast=0;int slow=0;
+        for(fast=0;fast<nums.size();fast++)
+        {
+            if(nums[fast]!=val)
+            {
+                swap(nums[slow],nums[fast]);
+                slow++;
+            }
+        }
+        return slow;
+     
+    }
+};
