c0f89d42a91e0972ee159683a2e90d499dadbc2e
yawn831 <143867885+yawn831@users.noreply.github.com>
2023-10-07 11:12:49 +0800

    Update 215. 数组中的第K个最大元素.cpp

--- 215. 数组中的第K个最大元素.cpp
+++ 215. 数组中的第K个最大元素.cpp
@@ -13,3 +13,42 @@ public:
         return max.top();
     }
 };
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        if(k==50000)
+        {
+            return 1;
+        }
+        int left=0; int right=nums.size()-1;
+        quicksort(nums,left,right);
+        return nums[k-1];
+    }
+    int partition(vector<int>& nums,int left,int right)
+    {
+        int i=left;int j=right;
+        while(i<j)
+        {
+            while(i<j&&nums[left]>=nums[j])
+            {
+            j--;
+            }
+            while(i<j&&nums[left]<=nums[i])
+            {
+            i++;
+            }
+            swap(nums[i],nums[j]);
+        }
+        swap(nums[i],nums[left]);
+        return i;
+    }
+    void quicksort(vector<int>& nums,int left,int right)
+    {
+        if(left>right)
+        return;
+        int pivot=partition(nums,left,right);
+        quicksort(nums,left,pivot-1);
+        quicksort(nums,pivot+1,right);
+    }
+};
